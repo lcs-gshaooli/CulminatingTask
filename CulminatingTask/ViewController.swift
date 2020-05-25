de2f643858b61a622ad886082a655ffed80ba17d
Gabriela Shaooli <gshaooli@lcs.on.ca>
2020-05-25 09:32:23 -0500

    Initial Commit

---
+++ CulminatingTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CulminatingTask
+//
+//  Created by Gabriela Shaooli on 2020-05-25.
+//  Copyright © 2020 Gabriela Shaooli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
